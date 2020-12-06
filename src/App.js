1ec1d7cf1c218666a37da432cc42bb6668505601
Your Name <you@example.com>
2020-12-06 14:43:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+
+import abc from "./img.jpg";
+
+
+
+class App extends React.Component {
+  constructor (props){
+    super(props)
+   this.state={
+        show : false,
+        count : 0,
+        fullName : 'Fedi Joubali',
+        profession: 'reactjs developper ',
+        bio: "It management",
+        profilePhoto: <img src='abc' alt='profilepicture'/>
+
+    };
+this.toggleDiv = this.toggleDiv.bind(this)
+    
+}
+
+oggleDiv = () =>{
+  const {show} = this.state;
+  this.setState({show : !show})
+}
+componentDidMount(){
+  this.myIInterval = setInterval(()=> this.tick(), 1000);
+}
+componentWillUnmount(){
+  clearInterval(this.myIInterval);
+}
+tick = ()=>{
+  this.setState(prevState => ({
+      count : prevState.count + 1 
+  }))
+}
+
+  render() {
+    const { count } = this.state 
+    return (
+      <div>
+                    <button onClick={()=> this.toggleDiv()}>show</button>
+            {this.state.show && 
+            <div className="container">
+              <p>{this.state.fullName}</p>
+               <p>{this.state.profession}</p>
+               <p>{this.state.bio}</p>
+               <img  src={abc} alt="profile-pic"/>
+               <p>current count: {count}</p>
+            </div>
+            
+             }
+
+
+      </div>
+    )
+  }
 }
 
 export default App;
